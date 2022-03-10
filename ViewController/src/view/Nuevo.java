83111b59d6b303425488040f5d2b23b6bca35516
joaquindoog7 <joakin.cor7@gmail.com>
2022-03-10 18:27:01 -0300

    Parcial_Final

---
+++ ViewController/src/view/Nuevo.java
@@ -0,0 +1,18 @@
+package view;
+
+import oracle.adf.view.rich.component.rich.nav.RichButton;
+
+public class Nuevo {
+    private RichButton nuevo;
+
+    public Nuevo() {
+    }
+
+    public void setNuevo(RichButton nuevo) {
+        this.nuevo = nuevo;
+    }
+
+    public RichButton getNuevo() {
+        return nuevo;
+    }
+}
